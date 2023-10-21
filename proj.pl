ed7b7d3106d1010e388522ff950ec39b6578d353
Ruben Esteves <92651281+Ruben38Esteves@users.noreply.github.com>
2023-10-22 00:50:24 +0100

    game loop started

--- proj.pl
+++ proj.pl
@@ -6,7 +6,7 @@ initial_state([
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ',' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ']
@@ -19,7 +19,7 @@ draw_board([H|T]) :-
     write('  -----------------------------\n'),
     draw_board([H|T], 1).
 
-draw_board(_, 7) :- !.
+draw_board(_, 8) :- !.
 
 draw_board([H|T], N) :-
     write(N),
@@ -39,9 +39,14 @@ draw_line([H|T]) :-
 
 /*place a piece in the board*/
 
-place_piece(Board, Player, Column, Line, NewBoard) :-
+place_piece(Board, Column, Line, NewBoard, 0) :-
     nth1(Line, Board, LineList),
-    replace(LineList, Column, Player, NewLineList),
+    replace(LineList, Column, 'X', NewLineList),
+    replace(Board, Line, NewLineList, NewBoard).
+
+place_piece(Board, Column, Line, NewBoard, 1) :-
+    nth1(Line, Board, LineList),
+    replace(LineList, Column, 'Y', NewLineList),
     replace(Board, Line, NewLineList, NewBoard).
 
 replace([_|T], 1, X, [X|T]) :- !.
@@ -57,4 +62,30 @@ remove_piece(Board, Column, Line, NewBoard) :-
 
 /*check if the game is over*/
 
+is_even(X) :-
+    0 is X mod 2.
 
+start_game(0).
+
+start_game(60):-
+    initial_state(B,Y),
+    draw_board(B),
+    write('Column: '),
+    read(C),
+    write('Line: '),
+    read(L),
+    place_piece(B, C, L, NB, 0),
+    NN is 60-1,
+    start_game(NN,NB).
+
+start_game(N,B):- N>0,
+    draw_board(B),
+    write('Column: '),
+    read(C),
+    write('Line: '),
+    read(L),
+    T is N mod 2,
+    place_piece(B, C, L, NB, T),
+    NN is N-1,
+    start_game(NN,NB).
+    
