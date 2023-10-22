36c9bf722f141f10ebe97d8c53a6079f5094a535
Ruben Esteves <92651281+Ruben38Esteves@users.noreply.github.com>
2023-10-22 01:32:16 +0100

    User input validation

--- proj.pl
+++ proj.pl
@@ -46,7 +46,7 @@ place_piece(Board, Column, Line, NewBoard, 0) :-
 
 place_piece(Board, Column, Line, NewBoard, 1) :-
     nth1(Line, Board, LineList),
-    replace(LineList, Column, 'Y', NewLineList),
+    replace(LineList, Column, 'O', NewLineList),
     replace(Board, Line, NewLineList, NewBoard).
 
 replace([_|T], 1, X, [X|T]) :- !.
@@ -70,22 +70,46 @@ start_game(0).
 start_game(60):-
     initial_state(B,Y),
     draw_board(B),
-    write('Column: '),
-    read(C),
-    write('Line: '),
-    read(L),
-    place_piece(B, C, L, NB, 0),
+    check_move(B,0, NB),
     NN is 60-1,
     start_game(NN,NB).
 
 start_game(N,B):- N>0,
     draw_board(B),
+    T is N mod 2,
+    check_move(B,T, NB),
+    NN is N-1,
+    start_game(NN,NB).
+
+check_move(B,T,NB):-
     write('Column: '),
     read(C),
     write('Line: '),
     read(L),
-    T is N mod 2,
-    place_piece(B, C, L, NB, T),
-    NN is N-1,
-    start_game(NN,NB).
+    nth1(L,B,Line),
+    nth1(C,Line,Elem),
+    check_move(B,C,L,Elem,T,NB).
     
+check_move(B,C,L,'X',T,NB):-
+    write('Invalid, try again'),nl,
+    write('Column: '),
+    read(NC),nl,
+    write('Line: '),
+    read(NL),nl,
+    nth1(NL,B,Line),
+    nth1(NC,Line,Elem),
+    check_move(B,NC,NL,Elem,T,NB).
+
+check_move(B,C,L,'O',T,NB):-
+    write('Invalid, try again'),nl,
+    write('Column: '),
+    read(NC),nl,
+    write('Line: '),
+    read(NL),nl,
+    nth1(NL,B,Line),
+    nth1(NC,Line,Elem),
+    check_move(B,NC,NL,Elem,T,NB).
+
+check_move(B,C,L,' ',T,NB):-
+    place_piece(B, C, L,NB, T),
+    write('Valid'),nl.
